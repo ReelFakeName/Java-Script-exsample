181c0ce03f93e3e672d9c5e1b348692d4204c5ca
ReelFakeName <71346555+ReelFakeName@users.noreply.github.com>
2020-09-15 14:31:56 -0400

    New Files 1.0 version

---
+++ script.js
@@ -0,0 +1,10 @@
+var boxEle = document.body.querySelector(".box");
+
+var UserInput = prompt("What is your name?")
+
+var numberone = 8
+var numbertwo = 2
+
+boxEle.innerHTML=UserInput;
+
+boxEle.innerHTML=numberone+numbertwo
